8942b12025bf9d63965d02f466b0f78459784989
Svetlana Doskochinskaia <Light3308@gmail.com>
2023-01-06 15:31:03 +0300

    HW #30

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
 "use strict";
-function shiftCipher(str, shift = 1) {
-    str = str.toLowerCase();
-    let result = '';
-    let charcode = 0;
+const firstShift = 'a'.charCodeAt(0);
+const lastShift = 'z'.charCodeAt(0);
+function shiftCipher(str, shift = -1) {
+    shift = shift % (lastShift - firstShift + 1);
+    let result = "";
     for (let i = 0; i < str.length; i++) {
-        charcode = str.charCodeAt(i) + shift;
-        result += String.fromCharCode(charcode);
+        let code = str.charCodeAt(i);
+        if (code < firstShift || code > lastShift) {
+            result += str[i];
+        }
+        else {
+            code += shift;
+            if (code > lastShift) {
+                code -= lastShift;
+                code += firstShift - 1;
+            }
+            result += String.fromCharCode(code);
+        }
     }
     return result;
 }
 console.log(shiftCipher("abc", 1));
-function shiftDecipher(str, shift = 1) {
-    str = str.toLowerCase();
-    let result = '';
-    let charcode = 0;
+console.log(shiftCipher("abz", 27));
+console.log(shiftCipher("abz", 1000));
+function shiftDecipher(str, shift = -1) {
+    shift = shift % (lastShift - firstShift + 1);
+    let result = "";
     for (let i = 0; i < str.length; i++) {
-        charcode = str.charCodeAt(i) - shift;
-        result += String.fromCharCode(charcode);
+        let code = str.charCodeAt(i);
+        if (code < firstShift || code > lastShift) {
+            result += str[i];
+        }
+        else {
+            code -= shift;
+            if (code > lastShift) {
+                code -= lastShift;
+                code += firstShift - 1;
+            }
+            result += String.fromCharCode(code);
+        }
     }
     return result;
 }
 console.log(shiftDecipher("bcd", 1));
+console.log(shiftDecipher("bca", 27));
+console.log(shiftDecipher("mnl", 1000));
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,31 +1,61 @@
+const firstShift: number = 'a'.charCodeAt(0);
+const lastShift: number = 'z'.charCodeAt(0);
 
-function shiftCipher(str: string, shift: number = 1): string {
-    
-    str = str.toLowerCase();
-    let result = '';
-    let charcode = 0;
-    for (let i = 0; i < str.length; i++) {
-  
-        charcode = str.charCodeAt(i) + shift;
-        result += String.fromCharCode(charcode);
+function shiftCipher(str: string, shift: number = -1): string {
+    shift = shift % (lastShift - firstShift + 1);
+
+    let result: string = "";
+
+    for (let i: number = 0; i < str.length; i++) {
+
+        let code: number = str.charCodeAt(i);
+
+        if (code < firstShift || code > lastShift) {
+            result += str[i];
+        } else {
+            code += shift; 
+            if (code > lastShift) {
+                code -= lastShift;
+                code += firstShift - 1;
+            }
+            result += String.fromCharCode(code);
+        }
     }
+
     return result;
-    
+
 }
+
 console.log(shiftCipher("abc", 1));
+console.log(shiftCipher("abz", 27));
+console.log(shiftCipher("abz", 1000));
 
 
-function shiftDecipher(str: string, shift: number = 1): string {
-    str = str.toLowerCase();
-    let result = '';
-    let charcode = 0;
-    for (let i = 0; i < str.length; i++) {
-  
-        charcode = str.charCodeAt(i) - shift;
-        result += String.fromCharCode(charcode);
+function shiftDecipher(str: string, shift: number = -1): string {
+    shift = shift % (lastShift - firstShift + 1);
+
+    let result: string = "";
+
+    for (let i: number = 0; i < str.length; i++) {
+
+        let code: number = str.charCodeAt(i);
+
+        if (code < firstShift || code > lastShift) {
+            result += str[i];
+        } else {
+            code -= shift; 
+            if (code > lastShift) {
+                code -= lastShift;
+                code += firstShift - 1;
+            }
+            result += String.fromCharCode(code);
+        }
     }
+
     return result;
 }
 
-console.log(shiftDecipher("bcd",1) )
+console.log(shiftDecipher("bcd", 1));
+console.log(shiftDecipher("bca", 27));
+console.log(shiftDecipher("mnl", 1000));
 
