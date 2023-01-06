8628295fea909730eac58ae4a48d3dd109076997
Svetlana Doskochinskaia <Light3308@gmail.com>
2023-01-06 18:36:19 +0300

    HW #30

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 "use strict";
 const firstShift = 'a'.charCodeAt(0);
 const lastShift = 'z'.charCodeAt(0);
-function shiftCipher(str, shift = -1) {
+function shiftCipher(str, shift = 1) {
+    return yesResult(str, shift);
+}
+function shiftDecipher(str, shift = 1) {
+    return yesResult(str, -shift);
+}
+function yesResult(str, shift = 1) {
     shift = shift % (lastShift - firstShift + 1);
     let result = "";
     for (let i = 0; i < str.length; i++) {
@@ -12,8 +18,7 @@ function shiftCipher(str, shift = -1) {
         else {
             code += shift;
             if (code > lastShift) {
-                code -= lastShift;
-                code += firstShift - 1;
+                code += (firstShift - lastShift) - 1;
             }
             result += String.fromCharCode(code);
         }
@@ -23,25 +28,6 @@ function shiftCipher(str, shift = -1) {
 console.log(shiftCipher("abc", 1));
 console.log(shiftCipher("abz", 27));
 console.log(shiftCipher("abz", 1000));
-function shiftDecipher(str, shift = -1) {
-    shift = shift % (lastShift - firstShift + 1);
-    let result = "";
-    for (let i = 0; i < str.length; i++) {
-        let code = str.charCodeAt(i);
-        if (code < firstShift || code > lastShift) {
-            result += str[i];
-        }
-        else {
-            code -= shift;
-            if (code > lastShift) {
-                code -= lastShift;
-                code += firstShift - 1;
-            }
-            result += String.fromCharCode(code);
-        }
-    }
-    return result;
-}
 console.log(shiftDecipher("bcd", 1));
 console.log(shiftDecipher("bca", 27));
 console.log(shiftDecipher("mnl", 1000));
--- app.ts
+++ app.ts
@@ -1,60 +1,38 @@
 const firstShift: number = 'a'.charCodeAt(0);
 const lastShift: number = 'z'.charCodeAt(0);
 
-function shiftCipher(str: string, shift: number = -1): string {
+function shiftCipher(str: string, shift: number = 1): string {
+    return yesResult(str, shift);
+
+}
+
+function shiftDecipher(str: string, shift: number = 1): string {
+    return yesResult(str, -shift);
+}
+
+function yesResult(str: string, shift: number = 1): string {
     shift = shift % (lastShift - firstShift + 1);
 
     let result: string = "";
 
     for (let i: number = 0; i < str.length; i++) {
-
         let code: number = str.charCodeAt(i);
-
         if (code < firstShift || code > lastShift) {
             result += str[i];
         } else {
-            code += shift; 
+            code += shift;
             if (code > lastShift) {
-                code -= lastShift;
-                code += firstShift - 1;
+                code += (firstShift - lastShift) - 1;
             }
             result += String.fromCharCode(code);
         }
     }
-
     return result;
-
 }
 
 console.log(shiftCipher("abc", 1));
 console.log(shiftCipher("abz", 27));
 console.log(shiftCipher("abz", 1000));
-
-
-function shiftDecipher(str: string, shift: number = -1): string {
-    shift = shift % (lastShift - firstShift + 1);
-
-    let result: string = "";
-
-    for (let i: number = 0; i < str.length; i++) {
-
-        let code: number = str.charCodeAt(i);
-
-        if (code < firstShift || code > lastShift) {
-            result += str[i];
-        } else {
-            code -= shift; 
-            if (code > lastShift) {
-                code -= lastShift;
-                code += firstShift - 1;
-            }
-            result += String.fromCharCode(code);
-        }
-    }
-
-    return result;
-}
-
 console.log(shiftDecipher("bcd", 1));
 console.log(shiftDecipher("bca", 27));
 console.log(shiftDecipher("mnl", 1000));
