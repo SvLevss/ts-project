0740172e6530725367d1dfd175216767ce83a859
Svetlana Doskochinskaia <Light3308@gmail.com>
2023-01-06 10:22:23 +0300

    HW #30

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 "use strict";
-// for (let i = 0; i < 3;i++){
-//     setTimeout(() => console.log(i)    )
-// };
-const hello = "world";
+function shiftCipher(str, shift = 1) {
+    str = str.toLowerCase();
+    let result = '';
+    let charcode = 0;
+    for (let i = 0; i < str.length; i++) {
+        charcode = str.charCodeAt(i) + shift;
+        result += String.fromCharCode(charcode);
+    }
+    return result;
+}
+console.log(shiftCipher("abc", 1));
+function shiftDecipher(str, shift = 1) {
+    str = str.toLowerCase();
+    let result = '';
+    let charcode = 0;
+    for (let i = 0; i < str.length; i++) {
+        charcode = str.charCodeAt(i) - shift;
+        result += String.fromCharCode(charcode);
+    }
+    return result;
+}
+console.log(shiftDecipher("bcd", 1));
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,4 +1,31 @@
-// for (let i = 0; i < 3;i++){
-//     setTimeout(() => console.log(i)    )
-// };
-const hello = "world";
+
+function shiftCipher(str: string, shift: number = 1): string {
+    
+    str = str.toLowerCase();
+    let result = '';
+    let charcode = 0;
+    for (let i = 0; i < str.length; i++) {
+  
+        charcode = str.charCodeAt(i) + shift;
+        result += String.fromCharCode(charcode);
+    }
+    return result;
+    
+}
+console.log(shiftCipher("abc", 1));
+
+
+function shiftDecipher(str: string, shift: number = 1): string {
+    str = str.toLowerCase();
+    let result = '';
+    let charcode = 0;
+    for (let i = 0; i < str.length; i++) {
+  
+        charcode = str.charCodeAt(i) - shift;
+        result += String.fromCharCode(charcode);
+    }
+    return result;
+}
+
+console.log(shiftDecipher("bcd",1) )
+
